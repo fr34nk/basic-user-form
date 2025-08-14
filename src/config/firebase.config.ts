35cda2414ad9d7f4ac7b35919ba6178fe0100102
frnk <2pr26fraz@mozmail.com>
2025-08-14 07:31:46 -0300

    fix: env variables

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -5,5 +5,5 @@ export default {
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGIN_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID || 'test-2e6a3',
+  appId: 'test-2e6a3',
 }
