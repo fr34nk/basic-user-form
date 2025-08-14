8b99e23fc165fb99cf3d2aa949278aed311e5f4f
frnk <2pr26fraz@mozmail.com>
2025-08-14 07:37:07 -0300

    fix: env varaibles

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -2,8 +2,8 @@ export default {
   apiKey: "AIzaSyCrfpwy79ZyL8ICPgGzrnc3ibrashBcJ5A",
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  projectId: "test-2e6a3",
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGIN_SENDER_ID,
-  appId: 'test-2e6a3',
+  appId: "1:154955321860:web:7ad0ba3e5bb7e273dd5b51"
 }
